3b56eba37ac6478c95ae813bd22e65a9143f3227
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-01-09 22:36:59 +0900

    Add thunk to async

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,24 +1,23 @@
-import {connect} from "react-redux";
-import {decrease, increase} from "../modules/counter";
+import { connect } from "react-redux";
+import { decreaseAsync, increaseAsync } from "../modules/counter";
 import Counter from "../components/Counter";
 
-function CounterContainer({number, increase, decrease}) {
-
-    return (
-        <Counter
-            number={number}
-            onIncrease={increase}
-            onDecrease={decrease}
-        />
-    );
+function CounterContainer({ number, increaseAsync, decreaseAsync }) {
+  return (
+    <Counter
+      number={number}
+      onIncrease={increaseAsync}
+      onDecrease={decreaseAsync}
+    />
+  );
 }
 
 export default connect(
-    state => ({
-        number: state.counter
-    }),
-    {
-        increase,
-        decrease
-    }
+  (state) => ({
+    number: state.counter,
+  }),
+  {
+    increaseAsync,
+    decreaseAsync,
+  }
 )(CounterContainer);
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,4 +1,4 @@
-import {createAction, handleActions} from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
 
 //action type 선언
 const INCREASE = "counter/INCREASE";
@@ -7,13 +7,28 @@ const DECREASE = "counter/DECREASE";
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// 1초 뒤에 increase || decrease 함수를 디스패치함
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
 
 //초기 상태 선언
 const initialState = 0;
 
-const counter = handleActions({
-    [INCREASE]: state => state + 1,
-    [DECREASE]: state => state - 1,
-}, initialState);
+const counter = handleActions(
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState
+);
 
 export default counter;
